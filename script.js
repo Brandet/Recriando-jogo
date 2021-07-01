e4dc266790d5c9a1f721a6ebac42545c8976f618
Brandet <paulobrandet@gmail.com>
2021-07-01 01:17:07 -0300

    definindo movimentos da cobrinha

--- script.js
+++ script.js
@@ -7,17 +7,47 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "rigth" ;
+
+/*Criando o Backgroud do jogo*/
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
-}
+} /*Fim Criando o Backgroud do jogo*/
 
+/*Criando a Cobrinha*/
 function criarCobrinha(){
     for(i=0; i < snake.length; i++) {
         context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+} /*Fim Criando a Cobrinha*/
+
+/*Criando a função iniciar jogo*/
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();/*Fim Criando a função iniciar jogo*/
+
+    let snakeX= snake[0].x;
+    let snakeY= snake[0].y;
+
+    /*Função coordenada*/
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    /*Fim Função coordenada*/
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+
 }
 
-criarBG();
-criarCobrinha();
+let jogo = setInterval(iniciarJogo, 100);
